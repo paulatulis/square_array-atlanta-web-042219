cf9cae2e37a2fb6f885135ff0d717de3d983b5c8
Paula Tulis <tulis.paula@gmail.com>
2019-04-10 17:15:45 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,12 @@
 def square_array(array)
   new_numbers = []
-  array.each do |x|
-    new_numbers << x ** 2
+  array.each {|x| new_numbers << x ** 2}
     new_numbers
 end
+end
+
+def square_array(array)
+ my_array = []
+  array.each {|i| my_array << i ** 2}
+  my_array
 end
